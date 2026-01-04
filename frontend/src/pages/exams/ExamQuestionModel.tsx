78f0843ca353db033fa5d3b0a9dca341e975a221
dunhhadka <dungthongoc2003@gmail.com>
2026-01-04 09:30:13 +0700

    fix

--- frontend/src/pages/exams/ExamQuestionModel.tsx
+++ frontend/src/pages/exams/ExamQuestionModel.tsx
@@ -62,9 +62,6 @@ export const ExamQuestionModel = ({
     }),
   ]
 
-  const [questionSelectedIds, setQuestionSelectedIds] =
-    useState<number[]>(selectedQuestionIds)
-
   const {
     searchTerm,
     setSearchTerm,
